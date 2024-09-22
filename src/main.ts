5f50b04078523326a5f58f69fc99e38ee4e9fb26
tomashm01 <tomashidalgomartin@gmail.com>
2024-09-22 23:26:06 +0200

    upload

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+  console.log('RUNNING ON PORT 3000');
+}
+bootstrap();
