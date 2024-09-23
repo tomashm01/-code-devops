4067b2642ca0a8257b8b421fc7607c07203bfb97
tomashm01 <tomashidalgomartin@gmail.com>
2024-09-23 13:47:31 +0200

    update port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('RUNNING ON PORT 3000');
+  await app.listen(80);
+  console.log('RUNNING ON PORT 80');
 }
 bootstrap();
